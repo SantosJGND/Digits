9bb071cb0624f3b3178b6a2fbeb6a73b03e81015
jgarcia <joaos3092@gmail.com>
2019-03-12 09:03:41 +0100

    functionalize. improve control over population sampling patterns.

--- Ideogram_tools.py
+++ Ideogram_tools.py
@@ -1,7 +1,27 @@
+import numpy as np
+import pandas as pd
+import re
+import scipy
+import itertools as it
+
+from sklearn.neighbors import KernelDensity
+from sklearn.decomposition import PCA
+from sklearn.model_selection import GridSearchCV
+from sklearn.cluster import MeanShift, estimate_bandwidth
+
+from matplotlib import pyplot as plt
+from matplotlib.collections import BrokenBarHCollection
+
+import collections
+
+def recursively_default_dict():
+        return collections.defaultdict(recursively_default_dict)
+
+#### reference KDE
 
 def extract_profiles(global_data,target_ind_dict):
     ## estimate the bandwith
-    params = {'bandwidth': np.linspace(np.min(global_data), np.max(global_data),20)}
+    params = {'bandwidth': np.linspace(np.min(global_data), np.max(global_data),25)}
     grid = GridSearchCV(KernelDensity(algorithm = "ball_tree",breadth_first = False), params,verbose=0)
     
     cluster_profiles= {x:[] for x in target_ind_dict.keys()}
@@ -23,75 +43,40 @@ def extract_profiles(global_data,target_ind_dict):
 
     
     return cluster_profiles
+
+
+def extract_profiles_class(global_data,target_ind_dict):
+    '''
+    copy of the previous function. change of name to deal with local 
+    function similarity.
+    '''
+    ## estimate the bandwith
+    cluster_profiles= recursively_default_dict()
+    params = {'bandwidth': np.linspace(np.min(global_data), np.max(global_data),20)}
+    grid = GridSearchCV(KernelDensity(algorithm = "ball_tree",breadth_first = False), params,verbose=0)
+    
+    combine= {}
+    for bull in target_ind_dict.keys():
+        
+        Quanted_set= global_data[target_ind_dict[bull],:]
+        grid.fit(Quanted_set)
+        kde = grid.best_estimator_
+        
+        P_dist = kde.score_samples(Quanted_set)
+        Fist = kde.score_samples(global_data)
+
+        ## Normalizing log-likelihood estimates by those of the reference set and extracting their cdf.
+        Fist = scipy.stats.norm(np.mean(P_dist),np.std(P_dist)).cdf(Fist)
+        cluster_profiles[bull]=Fist
+
+    
+    return cluster_profiles
     
 
-
-def Merge_class2(Ref_profiles,target_indx,focus_indicies,Out,Diff_threshold,X_threshold):
-    Blocks_genome = recursively_default_dict()
     
-    for CHR in Ref_profiles.keys():
-        print(CHR)
-        Points = sorted(Out[CHR].keys())
-        Likes = Ref_profiles[CHR]
-        
-        N_pops= len(Likes[[x for x in Likes.keys()][0]])
-        Pop_labels= Likes[[x for x in Likes.keys()][0]].keys()
-        print("number of reference populations: {0}".format(N_pops))
-        #Likes = {x:[Likes[bl][x] for bl in sorted(Likes.keys())] for x in Pop_labels}
-        #Likes = {x:np.array([y[0] for y in Likes[x]]) for x in Likes.keys()}
-
-        Topo = []
-        
-        #range_Parents = [x + Aro.shape[0] for x in range(Daddy.shape[0])]
-        #range_Crossed = [x for x in range(Aro.shape[0])]
-        
-        for bl in Likes.keys():
-            maxim= []
-            
-            
-            for acc in focus_indicies:
-                Test= [Likes[bl][x][0][acc] for x in sorted(Likes[bl].keys())]
-                
-                Guys= [Likes[bl][x][0][acc] for x in sorted(Likes[bl].keys())]
-                
-                if max(Test) <= X_threshold:
-                    maxim.append(N_pops)
-                else:
-                    Consex = [x for x in it.combinations(range(N_pops),2)]
-                    CL = []
-                    for j in Consex:
-                        Diff = [Guys[i] for i in j]
-                        
-                        if Guys.index(max(Guys)) not in j or len([x for x in Diff if x < X_threshold]) > 0:
-                            continue
-                        if max(Diff) <= X_threshold:
-                            Diff = 0
-                        else:
-#                            Diff = int(len([x for x in Diff if x <= Diff_threshold]) == 1)
-                            Diff = abs(max(Diff)) / abs(min(Diff))
-                            Diff = int(Diff > Diff_threshold)
-                        
-                        if Diff == 0:
-                            CL.append(j)
-                    
-                    if len(CL) == 2:
-                        maxim.append(7)
-                    if len(CL) == 1:
-                        maxim.append(sum(CL[0]) + N_pops)
-                    else:
-                        maxim.append(Guys.index(max(Guys)))
-            
-            Topo.append([maxim[c] + 1 for c in range(len(maxim))]) 
-        
-        Topo = np.array(Topo)
-        print(Topo.shape)
-        Clove = {CHR:{Points[x]:Topo[x,] for x in range(len(Points))}}
-        
-        Blocks_genome.update(Clove)
     
-    return Blocks_genome
-
-
+####################################################
+### Classification of KDE collections
 
 def Merge_class(Ref_profiles,focus_indicies,Out,Diff_threshold,X_threshold):
     Blocks_genome = recursively_default_dict()
@@ -168,13 +153,149 @@ def Merge_class(Ref_profiles,focus_indicies,Out,Diff_threshold,X_threshold):
     return Blocks_genome
 
 
-def compress_ideo(df,chromosome_list):
+def Merge_class2(Ref_profiles,target_indx,focus_indicies,Out,Diff_threshold,X_threshold):
+    Blocks_genome = recursively_default_dict()
     
+    for CHR in Ref_profiles.keys():
+        print(CHR)
+        Points = sorted(Out[CHR].keys())
+        Likes = Ref_profiles[CHR]
+        
+        N_pops= len(Likes[[x for x in Likes.keys()][0]])
+        Pop_labels= Likes[[x for x in Likes.keys()][0]].keys()
+        print("number of reference populations: {0}".format(N_pops))
+        #Likes = {x:[Likes[bl][x] for bl in sorted(Likes.keys())] for x in Pop_labels}
+        #Likes = {x:np.array([y[0] for y in Likes[x]]) for x in Likes.keys()}
+
+        Topo = []
+        
+        #range_Parents = [x + Aro.shape[0] for x in range(Daddy.shape[0])]
+        #range_Crossed = [x for x in range(Aro.shape[0])]
+        
+        for bl in Likes.keys():
+            maxim= []
+            
+            
+            for acc in focus_indicies:
+                Test= [Likes[bl][x][0][acc] for x in sorted(Likes[bl].keys())]
+                
+                Guys= [Likes[bl][x][0][acc] for x in sorted(Likes[bl].keys())]
+                
+                if max(Test) <= X_threshold:
+                    maxim.append(N_pops)
+                else:
+                    Consex = [x for x in it.combinations(range(N_pops),2)]
+                    CL = []
+                    for j in Consex:
+                        Diff = [Guys[i] for i in j]
+                        
+                        if Guys.index(max(Guys)) not in j or len([x for x in Diff if x < X_threshold]) > 0:
+                            continue
+                        if max(Diff) <= X_threshold:
+                            Diff = 0
+                        else:
+#                            Diff = int(len([x for x in Diff if x <= Diff_threshold]) == 1)
+                            Diff = abs(max(Diff)) / abs(min(Diff))
+                            Diff = int(Diff > Diff_threshold)
+                        
+                        if Diff == 0:
+                            CL.append(j)
+                    
+                    if len(CL) == 2:
+                        maxim.append(7)
+                    if len(CL) == 1:
+                        maxim.append(sum(CL[0]) + N_pops)
+                    else:
+                        maxim.append(Guys.index(max(Guys)))
+            
+            Topo.append([maxim[c] + 1 for c in range(len(maxim))]) 
+        
+        Topo = np.array(Topo)
+        print(Topo.shape)
+        Clove = {CHR:{Points[x]:Topo[x,] for x in range(len(Points))}}
+        
+        Blocks_genome.update(Clove)
+    
+    return Blocks_genome
+
+
+def Merge_class_mix(Ref_profiles,focus_indicies,Out,Diff_threshold,X_threshold):
+    Blocks_genome = recursively_default_dict()
+    Clove= recursively_default_dict()
+    for CHR in Ref_profiles.keys():
+        print(CHR)
+        Points = sorted(Out[CHR].keys())
+        Likes = Ref_profiles[CHR]
+        N_pops= len(Likes[[x for x in Likes.keys()][0]])
+        Pop_labels= Likes[[x for x in Likes.keys()][0]].keys()
+        print("number of reference populations: {0}".format(N_pops))
+        
+        Topo = []
+        
+        for bl in Likes.keys():
+            Guys = np.array([Likes[bl][x] for x in Pop_labels])
+            Guys = np.nan_to_num(Guys)
+            Guys = [[[y,0][int(y<=X_threshold)] for y in x] for x in Guys]
+            
+            Test = [int(x <= X_threshold) for x in np.amax(np.array(Guys),axis = 0)]     
+            
+            #
+            Guys = np.array(Guys).T
+            
+            maxim = np.argmax(Guys,axis = 1)
+            where_X = [x for x in range(Guys.shape[0]) if Test[x] == 1]
+            
+            #
+            Consex = [x for x in it.combinations(range(N_pops),2)]
+            if Consex:
+                for h in range(len(maxim)):
+                    CL = []
+                    for j in Consex:
+                        Diff = Guys[h,j]
+                        if maxim[h] not in j or len([x for x in Diff if x < X_threshold]) > 0:
+                            continue
+                        if max(Diff) <= X_threshold:
+                            Diff = 0
+                        else:
+                            Diff = abs(max(Diff)) / abs(min(Diff))
+                            Diff = int(Diff > Diff_threshold)
+                        
+                        if Diff == 0:
+                            CL.append(j)
+                    
+                    if len(CL) == 2:
+                        maxim[h] = 7
+                    if len(CL) == 1:
+                        maxim[h] = sum(CL[0]) + N_pops
+            
+            maxim[where_X] = N_pops
+            
+            if not Consex:
+                for h in range(len(maxim)):
+                    maxim[h] = int(10*Guys[h,0])    
+            
+            Clove[CHR][bl]= maxim + 1
+            Topo.append(maxim + 1)
+        
+        
+        Blocks_genome.update(Clove)
+    
+    return Blocks_genome
+
+
+#########################################################
+### Ideogram Processing
+
+
+def compress_ideo(df,chromosome_list, Out):
+    '''
+    Merge neighboring windows of the same class individual-wise. Returns pandas df.
+    '''
     new_set = []
     
     for CHR in range(len(chromosome_list)):
         
-        Chr = int(re.search('Region_(.+?)_',chromosome_list[CHR]).group(1))
+        Chr = int(re.search('chr(.+?)_',chromosome_list[CHR]).group(1))
         sub = df[df.chrom == chromosome_list[CHR]]
         Coordinates = sorted(sub.start)
         Size = sub.shape[0]
@@ -202,6 +323,7 @@ def compress_ideo(df,chromosome_list):
     
     new_set = pd.DataFrame(new_set,columns = ['chrom', 'start', 'end', 'gieStain'])
     return new_set
+
 
 
 # Here's the function that we'll call for each dataframe (once for chromosome
@@ -238,7 +360,8 @@ def chromosome_collections(df, y_positions, height,  **kwargs):
         del df['width']
 
 
-def return_ideogram(ideo, out= True):
+
+def return_ideogram(ideo, chromosome_list,ID,out= True,height=30,width= 10):
     # Height of each ideogram
     chrom_height = 1
 
@@ -288,7 +411,6 @@ def return_ideogram(ideo, out= True):
 
     # Colors for different chromosome stains
     color_lookup = {
-        'white': [255,255,255],
         'red': [255, 0, 0],
         'yellow': [255, 255, 0],
         'blue': [0, 0, 255],
@@ -306,7 +428,7 @@ def return_ideogram(ideo, out= True):
     ideo['width'] = ideo.end - ideo.start
 
     # Width, height (in inches)
-    figsize = (10, 30)
+    figsize = (width, height)
 
     fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111)
@@ -327,7 +449,74 @@ def return_ideogram(ideo, out= True):
     ax.set_yticklabels(chromosome_list, fontsize = 5)
     ax.axis('tight')
     if out == True:
-        plt.savefig('Ideo_step_' + '_OutlierTh' + str(Outlier_threshold) + '_Z' +str(Comparison_threshold)+ '.png',bbox_inches = 'tight')
+        plt.savefig('Ideo_step_' + '_' + ID + '.png',bbox_inches = 'tight')
     return fig
 
 
+def KDE_windows(Windows,label_vector,ref_labels,Out,colors= 'standard',alt_col= [],n_comps= 5,Comparison_threshold= 4,Outlier_threshold= 1e-3,return_var= True):
+
+    target_indx= {z:[x for x in range(len(label_vector)) if label_vector[x] == z] for z in ref_labels}
+
+    Windows_profiles= recursively_default_dict()
+    
+    var_comp_store= []
+    
+    pca2 = PCA(n_components=n_comps, whiten=False,svd_solver='randomized')
+
+    for bl in Windows[1].keys():
+
+        data= Windows[1][bl]
+        data= pca2.fit_transform(data)
+        
+        local_pcvar= list(pca2.explained_variance_ratio_)
+        
+        local_pcvar= [bl,*local_pcvar]
+        
+        var_comp_store.append(local_pcvar)
+        
+        profiles= extract_profiles(data,target_indx)
+
+        ### store stuff.
+        Windows_profiles[1][bl]= profiles
+    
+    
+    var_comp_store= np.array(var_comp_store)
+    var_comp_store= pd.DataFrame(var_comp_store,columns=['set',*['PC' + str(x + 1) for x in range(n_comps)]])
+
+    focus_indexes= [x for x in range(len(label_vector))]
+    if colors== 'standard':
+        color_ref= ['red','yellow','blue','black','green','orange','purple','green','silver','red3','deepskyeblue','navy','chartreuse','darkorchid3','goldenrod2']
+    else:
+        color_ref= alt_col
+    Blocks = Merge_class(Windows_profiles,focus_indexes,Out,Comparison_threshold,Outlier_threshold)
+
+    Ideo_KDE = []
+    chromosome_list= []
+    chromosomes= Blocks.keys()
+
+    for here in range(len(label_vector)):
+        Subject = 'sample' + str(here)
+
+        chromosome_list.extend(['Region_chr'+str(Chr)+ '_' + Subject for Chr in chromosomes])
+
+        Stock = [[['Region_chr'+str(Chr)+ '_' + Subject,bl,Out[Chr][bl],color_ref[Blocks[Chr][bl][here] - 1]] for bl in sorted(Blocks[Chr].keys())] for Chr in chromosomes]
+        Stock = [y for y in it.chain(*[z for z in it.chain(*[Stock])])]
+
+        Ideo_KDE.extend(Stock)
+
+    #### begin by compressing assignments by individuals. Lightens the load of the following plot.
+    import re
+    ideo_kde = pd.DataFrame(Ideo_KDE,columns = ['chrom', 'start', 'end', 'gieStain'])
+
+    # Filter out chromosomes not in our list
+    ideo_kde = ideo_kde[ideo_kde.chrom.apply(lambda x: x in chromosome_list)]
+
+    ideo_kde = compress_ideo(ideo_kde,chromosome_list,Out)
+
+    ID= 'kde' 
+    
+    if return_var:
+        return ideo_kde,chromosome_list, ID, var_comp_store
+    else:
+        return ideo_kde,chromosome_list, ID
+        
